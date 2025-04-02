847e3d489ec8aac1cfb32c93d466e5f9773af1e5
min1k <masyuk0tanika@gmail.com>
2025-04-02 05:14:17 +0300

    Hotfix calculation limited to 3 terms

--- funcA.cpp
+++ funcA.cpp
@@ -1,6 +1,8 @@
 #include "funcA.h"
 
 double FuncA::calculate(){
-return 0;
+	return tgamma(1) / (pow(4, 0) * pow(tgamma(0 + 1), 2) * 1) +
+	tgamma(3) / (pow(4, 1) * pow(tgamma(1 + 1), 2) * 3) +
+	tgamma(5) / (pow(4, 2) * pow(tgamma(2 + 1), 2) * 5);
 }
 
