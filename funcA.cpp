7f495b097f715f87ce63afe521ff71cd6726e912
min1k <masyuk0tanika@gmail.com>
2025-04-02 05:46:10 +0300

    Final

--- funcA.cpp
+++ funcA.cpp
@@ -1,6 +1,10 @@
 #include "funcA.h"
-
-double FuncA::calculate(){
-return 0;
+double FuncA::calculate(int n){
+	double sum = 0;
+	for(int k = 0; k < n; k++){
+		double term = tgamma(2 * k + 1) / (pow(4, k) + pow(tgamma(k + 1), 2) * (2 * k + 1));
+		sum += term;
+	}
+	return 0;
 }
 
